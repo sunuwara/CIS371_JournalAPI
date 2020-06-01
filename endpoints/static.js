3c41a9a2a9adf0fdd2ab7a732815a5124e222e4f
sunuwara <sunuwara@mail.gvsu.edu>
2020-05-31 21:06:12 -0400

    adding static content endpoint

--- endpoints/static.js
+++ endpoints/static.js
@@ -1,3 +1,35 @@
 /* static endpoints in this file */
 
-// TODO
+module.exports = function (app) {
+  var tools = require("../helper/tools.js");
+
+  // Get static content information with given the id. 200 on Success. 404 if not found.
+  app.get("/static/:id", (req, res) => {
+    tools.getEntity(req, res, "static");
+  });
+
+  // Create a blank new static content. 200 on Success. 400 if error with request.
+  app.post("/static", (req, res) => {
+    // Create a new static
+    var staticContent = {
+      url: "",
+    };
+
+    tools.createEntity(req, res, "static", staticContent);
+  });
+
+  // Update static content with the given id. 200 on Success. 404 if not found.
+  app.put("/static/:id", (req, res) => {
+    // Create a new static content
+    var staticContent = {
+      url: req.body.url || "",
+    };
+
+    tools.updateEntity(req, res, "static", staticContent);
+  });
+
+  // Delete static content with the given id. 200 on Success. 404 if not found.
+  app.delete("/static/:id", (req, res) => {
+    tools.deleteEntity(req, res, "static");
+  });
+};
